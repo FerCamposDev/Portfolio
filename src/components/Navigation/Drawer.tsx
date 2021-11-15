eca9afc4783f473feb432746a9631c9da6b1d709
FerCamposDev <fernando.developments@gmail.com>
2021-11-15 18:34:56 -0300

    organizo navigation

--- src/components/Navigation/Drawer.tsx
+++ src/components/Navigation/Drawer.tsx
@@ -3,26 +3,22 @@ import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import { Button, Grid, Hidden } from '@material-ui/core';
-import { Link } from 'react-router-dom'
 
 import HomeIcon from '@material-ui/icons/Home';
 import AppsIcon from '@material-ui/icons/Apps';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
 import ContactMailIcon from '@material-ui/icons/ContactMail';
 import { useTranslation } from 'react-i18next';
-import SwitchIdioma from '../SwitchIdioma';
+import SwitchIdioma from './SwitchIdioma';
+import MyAppBar from './MyAppBar';
 
 const drawerWidth = 240;
 
@@ -30,26 +26,6 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     backgroundColor: "#ABE9F4",
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  hide: {
-    display: 'none',
   },
   drawer: {
     width: drawerWidth,
@@ -82,15 +58,6 @@ const useStyles = makeStyles((theme) => ({
     }),
     marginLeft: 0,
   },
-  divider: {
-    width: "3px",
-    marginRight: theme.spacing(1),
-    marginLeft: theme.spacing(1),
-  },
-  linkButton: {
-    textDecoration: 'none',
-    color: 'white',
-  }
 }));
 
 function ListItemLink(props: any) {
@@ -104,7 +71,6 @@ export default function PersistentDrawerLeft() {
   const [open, setOpen] = React.useState(false);
   let lang = "/" + localStorage.getItem("lang") || "";
 
-
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -116,69 +82,7 @@ export default function PersistentDrawerLeft() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-
-
-          <Grid container>
-            <Link to="/inicio"
-              className={classes.linkButton}
-            >
-              <Button color='inherit'>
-                {t('menu.home')}
-              </Button>
-            </Link>
-          </Grid>
-
-          <Hidden smDown>
-
-            <Grid container justify="flex-end" alignItems="center">
-              <Link to="/curriculum"
-                className={classes.linkButton}
-              >
-                <Button color='inherit'>
-                  {t('menu.resume')}
-                </Button>
-              </Link>
-              <Link to="/proyectos"
-                className={classes.linkButton}
-              >
-                <Button color='inherit'>
-                  {t('menu.projects')}
-                </Button>
-              </Link>
-              <Link to="/contacto"
-                className={classes.linkButton}
-              >
-                <Button color='inherit'>
-                  {t('menu.contact')}
-                </Button>
-              </Link>
-
-              <Divider orientation="vertical" flexItem className={classes.divider} />
-
-              <Grid item>
-                <SwitchIdioma />
-              </Grid>
-            </Grid>
-          </Hidden>
-
-        </Toolbar>
-      </AppBar>
+      <MyAppBar open={open} handleDrawerOpen={handleDrawerOpen}/>
       <Drawer
         className={classes.drawer}
         anchor="left"
@@ -186,6 +90,7 @@ export default function PersistentDrawerLeft() {
         classes={{
           paper: classes.drawerPaper,
         }}
+        onClick={handleDrawerClose}
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
--- src/components/Navigation/MyAppBar.tsx
+++ src/components/Navigation/MyAppBar.tsx
@@ -1,83 +1,109 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Divider, Grid, Hidden, Link } from '@mui/material';
+import clsx from 'clsx';
+import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Button, Grid, Hidden } from '@material-ui/core';
 
+import SwitchIdioma from './SwitchIdioma';
 import { useTranslation } from 'react-i18next';
-import SwitchIdioma from '../SwitchIdioma';
 
-interface MyAppBarProps {
-  open?: boolean;
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  divider: {
+    width: "3px",
+    marginRight: theme.spacing(1),
+    marginLeft: theme.spacing(1),
+  },
+  linkButton: {
+    textDecoration: 'none',
+    color: 'white',
+  }
+}));
+interface Props {
+  open: boolean
   handleDrawerOpen: () => void
 }
 
-export default function MyAppBar(props: MyAppBarProps) {
-  const { open, handleDrawerOpen } = props;
+const MyAppBar = ({ open, handleDrawerOpen }: Props) => {
   const { t } = useTranslation();
+  const classes = useStyles();
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-            onClick={handleDrawerOpen}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Grid container>
-            <Link href="/inicio"
-              component={Button}
-              color="inherit"
-            >
-              <Typography variant="h6">
-                {t('menu.home')}
-              </Typography>
-            </Link>
-          </Grid>
-          <Hidden mdDown>
-            <Grid container justifyContent="flex-end" alignItems="center">
-              <Link href="/curriculum"
-                component={Button}
-                color="inherit"
-              >
+    <AppBar
+      position="fixed"
+      className={clsx(classes.appBar, {
+        [classes.appBarShift]: open,
+      })}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          onClick={handleDrawerOpen}
+          edge="start"
+          className={clsx(classes.menuButton, open && classes.hide)}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Grid container>
+          <Link to="/inicio" className={classes.linkButton} >
+            <Button color='inherit'>
+              {t('menu.home')}
+            </Button>
+          </Link>
+        </Grid>
+
+        <Hidden smDown>
+          <Grid container justify="flex-end" alignItems="center">
+            <Link to="/curriculum" className={classes.linkButton} >
+              <Button color='inherit'>
                 {t('menu.resume')}
-              </Link>
-              <Link href="/proyectos"
-                component={Button}
-                color="inherit"
-
-              >
+              </Button>
+            </Link>
+            <Link to="/proyectos" className={classes.linkButton} >
+              <Button color='inherit'>
                 {t('menu.projects')}
-              </Link>
-              <Link href="/contacto"
-                component={Button}
-                color="inherit"
-              >
+              </Button>
+            </Link>
+            <Link to="/contacto" className={classes.linkButton} >
+              <Button color='inherit'>
                 {t('menu.contact')}
-              </Link>
-
-              <Divider orientation="vertical" flexItem sx={{
-                width: "3px",
-                marginRight: "5px",
-                marginLeft: "5px",
-              }} />
-
-              <Grid item>
-                <SwitchIdioma />
-              </Grid>
+              </Button>
+            </Link>
+            <Divider orientation="vertical" flexItem className={classes.divider} />
+            <Grid item>
+              <SwitchIdioma />
             </Grid>
-          </Hidden>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+          </Grid>
+        </Hidden>
+      </Toolbar>
+    </AppBar>
+  )
 }
+
+export default MyAppBar;
